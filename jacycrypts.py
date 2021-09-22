7139ecc78de0d35c0da1ad02ca0af9075cc63027
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-09-22 19:11:19 -0300

    Adicionando verify_hash e hash SHA256

--- jacycrypts.py
+++ jacycrypts.py
@@ -4,29 +4,45 @@ import hashlib
 
 class JacyCrypt:
     BLOCK_SIZE = 32
-    
-    def __init__(self, valor_secreto):
+
+    HASHS = {
+        'MD5': {'f': hashlib.md5, 'tam': 32},
+        'SHA256': {'f': hashlib.sha256, 'tam': 64},
+    }
+
+    def __init__(self, valor_secreto, type_hash='MD5'):
+        type_hash = type_hash.upper()
         self.valor_secreto = valor_secreto
-        self.key = hashlib.md5(valor_secreto.encode()).digest()
+        self.type_hash = self.HASHS[type_hash]
+        self.key = self.generate_key(valor_secreto, type_hash)
 
     def encrypt(self, mensagem):
-        hash = self.gerar_hash(mensagem)
+        hash = self.generate_hash(mensagem)
         cipher = DES3.new(self.key, DES3.MODE_ECB)
         msg_pad = pad(f'{mensagem}{hash}'.encode(), self.BLOCK_SIZE)
         msg = cipher.encrypt(msg_pad)
         return msg
-    
-    def decrypt(self,mensagem):
+
+    def decrypt(self, mensagem):
         cipher = DES3.new(self.key, DES3.MODE_ECB)
         msgd_pad = cipher.decrypt(mensagem)
         msgdhash = unpad(msgd_pad, self.BLOCK_SIZE).decode()
         
-        # Separa a mensagem da hash, considerando que o MD5 tem 128 bits
-        hash = msgdhash[-32:]
-        msgd = msgdhash[:-32]
+        # Separa a mensagem da hash, considerando que os bytes da hash
+        tam_hash = self.type_hash['tam']
+        hash = msgdhash[-tam_hash:]
+        msgd = msgdhash[:-tam_hash]
         return msgd, hash
 
-    def gerar_hash(self, mensagem):
+    def generate_hash(self, mensagem):
         mensagem = f'{mensagem}{self.valor_secreto}'.encode()
-        hash = hashlib.md5(mensagem).hexdigest()
+        hash = self.type_hash['f'](mensagem).hexdigest()
         return hash
+
+    def generate_key(self, valor, type_hash):
+        hash = self.type_hash['f'](valor.encode()).digest()[:24]
+        return hash
+
+    def verify_hash(self, mensagem, hash):
+        hash_new = self.generate_hash(mensagem)
+        return hash_new == hash
