0f1cbdde851c131afe793cd73952d84bb3263025
Jacyiricê Silva Oliveira <jacyirice@gmail.com>
2021-09-22 19:11:59 -0300

    adicionando type_hash

--- main.py
+++ main.py
@@ -6,15 +6,13 @@ mensagem_original = 'Jacyirice Silva Oliveira'
 print('Mensagem original:', mensagem_original)
 
 # Encriptar
-jacycrypt = JacyCrypt(valor_secreto)
+jacycrypt = JacyCrypt(valor_secreto, type_hash='sha256')
 mensagem_encriptada = jacycrypt.encrypt(mensagem_original)
 print('Mensagem encriptada:', mensagem_encriptada)
 
 # Decriptar
-jacycrypt = JacyCrypt(valor_secreto)
+jacycrypt = JacyCrypt(valor_secreto, type_hash='sha256')
 mensagem_decriptada, hash = jacycrypt.decrypt(mensagem_encriptada)
-hash_new = jacycrypt.gerar_hash(mensagem_decriptada)
 print('Mensagem decriptada:', mensagem_decriptada)
 print('Hash da mensagem encriptada:', hash)
-print('As hashs são iguais?', hash_new == hash)
-
+print('As hashs são iguais?', jacycrypt.verify_hash(mensagem_decriptada, hash))
